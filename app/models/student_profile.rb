3da77841349bec3de520c6d53033934a69595467
Dovran <dovran@MacBook-Air-Dovran.local>
2024-09-02 17:55:54 +0500

    setup annotate gem

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: student_profiles
+#
+#  id                 :bigint           not null, primary key
+#  interests          :text
+#  level_of_knowledge :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  user_id            :bigint           not null
+#
+# Indexes
+#
+#  index_student_profiles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class StudentProfile < ApplicationRecord
   belongs_to :user
 
--- spec/factories/teacher_profiles.rb
+++ spec/factories/teacher_profiles.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: teacher_profiles
+#
+#  id             :bigint           not null, primary key
+#  experience     :text
+#  specialization :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_id        :bigint           not null
+#
+# Indexes
+#
+#  index_teacher_profiles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :teacher_profile do
     user { nil }
